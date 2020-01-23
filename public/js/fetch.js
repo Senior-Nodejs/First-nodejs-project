ef301e90ef9f0994fd710f9b66784bd0e8e285c5
ahmed <ahmed.aljfry05@gmail.com>
2020-01-23 13:29:52 +0200

    fixing bugs

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -2,12 +2,14 @@ const form = document.querySelector('form')
 const input = document.querySelector('input')
 const message = document.querySelector('#message')
 
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = input.value
+    console.log('submitted')
     fetch(`http://localhost:4000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
-            message.textContent = "location :" + " " + data.location
+            message.textContent = "location : " + data.location
         })
     })
 })
